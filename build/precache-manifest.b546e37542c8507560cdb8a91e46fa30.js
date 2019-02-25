8cfdef0a7c688eda5c2a70aa73629291306ac3f3
Sebastian Kondratowicz <feeverpl@gmail.com>
2019-02-25 06:36:06 +0100

    build added

---
+++ build/precache-manifest.b546e37542c8507560cdb8a91e46fa30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "3f2ab8a9b22f9a65aaf4",
+    "url": "./static/js/main.3f2ab8a9.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "./static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "3f2ab8a9b22f9a65aaf4",
+    "url": "./static/css/main.633091b9.chunk.css"
+  },
+  {
+    "revision": "f7654cb2cd266a25e1d927525a2f6d94",
+    "url": "./index.html"
+  }
+];
